b60f591e518aa73846fb129595b2423a5ed9d973
Carlos Andres Perez <carlosaperez1997@gmail.com>
2019-06-03 12:11:17 -0500

    secreo las rutas de follow

--- backend/controllers/follow.js
+++ backend/controllers/follow.js
@@ -10,6 +10,44 @@ let FollowController = {
     index: (req, res) => {
         res.status(200).send({ message: 'hola mundo' });
     },
+    store: (req, res) => {
+        let params = req.body;
+        let follow = new Follow();
+        console.log(req.user);
+        follow.user = req.user.sub;
+        follow.followed = params.followed;
+        follow.save((err, followStored) => {
+            if (err) return res.status(500).send({ errors: `Error en el servidor: ${err}` });
+            if (!followStored) return res.status(404).send({ errors: 'el seguimiento no se ha guardado' });
+            return res.status(200).send(followStored);
+        });
+    },
+    destroy: (req, res) => {
+        Follow.find({ user: req.user.sub, followed: req.params.id }).remove(err => {
+            if (err) return res.status(500).send({ errors: `Error en el servidor: ${err}` });
+            return res.status(200).send({ message: 'se ha eliminado correctamente' });
+        });
+    },
+    getFollowingUsers: (req, res) => {
+        let user = (req.params.id) ? req.params.id : req.user.sub;
+        let page = (req.params.page) ? req.params.page : 1;
+        let itemPerPage = 10;
+        Follow.find({ user }).populate({ path: 'followed' }).paginate(page, itemPerPage, (err, follows, total) => {
+            if (err) return res.status(500).send({ errors: `Error en el servidor: ${err}` });
+            if (!follows) return res.status(400).send({ errors: 'No estas siguiendo a ningun usuario' });
+            return res.status(200).send({ total, pages: Math.ceil(total / itemPerPage), follows });
+        });
+    },
+    getFollowedUsers: (req, res) => {
+        let user = (req.params.id) ? req.params.id : req.user.sub;
+        let page = (req.params.page) ? req.params.page : 1;
+        let itemPerPage = 10;
+        Follow.find({ followed: user }).populate({ path: 'user' }).paginate(page, itemPerPage, (err, follows, total) => {
+            if (err) return res.status(500).send({ errors: `Error en el servidor: ${err}` });
+            if (!follows) return res.status(400).send({ errors: 'No estas siguiendo a ningun usuario' });
+            return res.status(200).send({ total, pages: Math.ceil(total / itemPerPage), follows });
+        });
+    }
 };
 
 module.exports = FollowController;
--- backend/routes/follow.js
+++ backend/routes/follow.js
@@ -12,6 +12,9 @@ let md_auth = require('../middlewares/authenticate');
 let api = express.Router();
 
 api.get('/', FollowController.index);
-
+api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowedUsers);
+api.get('/show/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
+api.post('/', md_auth.ensureAuth, FollowController.store);
+api.delete('/:id', md_auth.ensureAuth, FollowController.destroy);
 
 module.exports = api;
